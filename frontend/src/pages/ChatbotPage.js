d1d7028fa59d68e4185ee8c67c22e3c71ae75c88
V.Akhila <vadlaakhila122@gmail.com>
2025-03-20 01:19:49 +0530

    problem slove1

--- frontend/src/pages/ChatbotPage.js
+++ frontend/src/pages/ChatbotPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://med-bridge-tawny.vercel.app ';
 
 const ChatbotPage = () => {
   const [messages, setMessages] = useState([]);
